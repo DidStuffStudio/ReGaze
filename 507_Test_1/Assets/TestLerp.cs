f95fee70f868d864654e886dcce3f3b6dc27624f
balazsivanyi <bi@indieframe.com>
2020-11-11 20:08:40 +0100

    State shaders

---
+++ 507_Test_1/Assets/TestLerp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLerp : MonoBehaviour
+{
+    public Material baseMaterial;
+    public Material selectedMaterial;
+
+    private MeshRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
