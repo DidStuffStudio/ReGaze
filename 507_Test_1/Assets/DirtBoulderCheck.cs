8b293c2a6213f6b9bf2dfbca7cadb0612b6703b3
Snaylo <42110788+ScottJamesNaylor@users.noreply.github.com>
2020-11-18 12:34:31 +0100

    Obstacles in place

---
+++ 507_Test_1/Assets/DirtBoulderCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtBoulderCheck : MonoBehaviour
+{
+   public Transform boulder, yPass;
+   public GameObject jumpCollider;
+
+   private void Start()
+   {
+      jumpCollider.SetActive(false);
+   }
+
+   private void Update()
+   {
+      if (boulder.position.y < yPass.position.y)
+      {
+         jumpCollider.SetActive(true);
+      }
+      
+   }
+}
