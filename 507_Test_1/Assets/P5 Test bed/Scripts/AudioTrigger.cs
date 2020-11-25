d628ea45f45cd873b2eeaff26386bf56bd1e0482
Snaylo <42110788+ScottJamesNaylor@users.noreply.github.com>
2020-11-25 22:15:20 +0100

    Scene two environment draft 2

---
+++ 507_Test_1/Assets/P5 Test bed/Scripts/AudioTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioClip audioToPlay;
+    public UnityEvent OnTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 12)
+        {
+            var src = GetComponent<AudioSource>();
+            src.clip = audioToPlay;
+            src.Play();
+            OnTrigger?.Invoke();
+
+        }
+    }
+}
---
+++ 507_Test_1/Assets/P5 Test bed/Scripts/Usability_StoryManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usability_StoryManager : MonoBehaviour
+{
+
+    public AudioSource voiceoverSource;
+    public AudioClip[] voiceClips = new AudioClip[8];
+
+    public bool jumped;
+    private bool hasJumpedFirstTime;
+
+
+    public void PlayVoice(int part)
+    {
+        voiceoverSource.clip = voiceClips[part];
+        voiceoverSource.PlayOneShot(voiceClips[part]);
+    }
+
+    private void Update()
+    {
+        if (jumped && !hasJumpedFirstTime)
+        {
+            PlayVoice(2);
+            hasJumpedFirstTime = true;
+        }
+    }
+}
